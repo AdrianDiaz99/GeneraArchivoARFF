374dd9e81036da975c6b5d794021ade4f17b4406
AdrianDiaz99 <jesusadriandiazorozco@gmail.com>
2020-12-12 13:30:39 -0700

    Logica Se Contagio

--- src/generaarchivoarff/GeneraArchivoARFF.java
+++ src/generaarchivoarff/GeneraArchivoARFF.java
@@ -1,6 +1,7 @@
 package generaarchivoarff;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JOptionPane;
 import javax.swing.plaf.metal.MetalIconFactory;
@@ -13,7 +14,18 @@ public class GeneraArchivoARFF {
 
     BufferedWriter bw;
     private final int TOTAL_LINEAS = 100000;
+    Random r = new Random();
+    final String [] 
+            EVENTOS             = {"Social","Familiar","Negocio","Deportivo"},
+            TIPOS_LUGAR_EVENTOS = {"Cerrado","AireLibre","SemiAbierto"},
+            SI_O_NO             = {"Si","No"},
+            DIAS                = {"LunesViernes","SabadoDomingo"},
+            GENEROS             = {"Masculino","Femenino"};
 
+    public static void main(String[] args) {
+        new GeneraArchivoARFF();
+    }
+    
     GeneraArchivoARFF() {
         try {
             generaArchivo();
@@ -38,24 +50,19 @@ public class GeneraArchivoARFF {
 
     public String generaInformacion() {
         String res = "";
-        Random r = new Random();
         int edad = r.nextInt(100) + 1, hora = r.nextInt(24) + 1, noPersonas = r.nextInt(99) + 2;
         String nivel = "", dia = "", turno = "", evento = "", tipoLugarEvento = "", sanaDistancia = "", 
                 usoCubrebocas = "", genero = "", seContagio = "";
 
-        if (edad <= 12) {
+        if (edad <= 12)
             nivel = "Niño";
-        }
-        if (edad > 12 && edad <= 18) {
+        else if (edad <= 18)
             nivel = "Joven";
-        }
-        if (edad > 18 && edad <= 70) {
+        else if (edad <= 70)
             nivel = "Adulto";
-        }
-        if (edad > 70 && edad <= 100) {
+        else
             nivel = "Anciano";
-        }
-
+        
         if(hora >= 5 && hora < 12)
                 turno = "Matutino";
         if(hora >= 12 && hora < 19)
@@ -63,42 +70,13 @@ public class GeneraArchivoARFF {
         if(hora >= 19 || hora < 5)
                 turno = "Nocturno";
 
-        int control;
-
-        control = r.nextInt(4);
-        switch (control) {
-            case 0:
-                evento = "Social";
-                break;
-            case 1:
-                evento = "Familiar";
-                break;
-            case 2:
-                evento = "Negocio";
-                break;
-            case 3:
-                evento = "Deportivo";
-                break;
-        }
-
-        control = r.nextInt(3);
-        switch (control) {
-            case 0:
-                tipoLugarEvento = "Cerrado";
-                break;
-            case 1:
-                tipoLugarEvento = "AireLibre";
-                break;
-            case 2:
-                tipoLugarEvento = "SemiAbierto";
-                break;
-        }
-
-        sanaDistancia = r.nextInt(2) == 0 ? "Si" : "No";
-        usoCubrebocas = r.nextInt(2) == 0 ? "Si" : "No";
-        seContagio = r.nextInt(2) == 0 ? "Si" : "No";
-        dia = r.nextInt(2) == 0 ? "LunesViernes" : "SabadoDomingo";
-        genero = r.nextInt(2) == 0 ? "Masculino" : "Femenino";
+        evento          = elegirElementoRandom(EVENTOS);
+        tipoLugarEvento = elegirElementoRandom(TIPOS_LUGAR_EVENTOS);
+        sanaDistancia   = elegirElementoRandom(SI_O_NO);
+        usoCubrebocas   = elegirElementoRandom(SI_O_NO);
+        dia             = elegirElementoRandom(DIAS);
+        genero          = elegirElementoRandom(GENEROS);
+        seContagio      = calculaSeContagio(tipoLugarEvento, sanaDistancia, usoCubrebocas, dia);
         
         res = edad + "," + nivel + "," + dia + "," + turno + "," + hora + "," + 
                 evento + "," + tipoLugarEvento + "," + noPersonas + "," + 
@@ -106,12 +84,45 @@ public class GeneraArchivoARFF {
         
         return res;
     }
+    
+    private String calculaSeContagio(String tipoLugarEvento, String sanaDistancia, String usoCubrebocas, String dia) {
+       ArrayList<String> data = new ArrayList();
+       
+       for(int i = 0; i < 20 ; i++){
+           data.add("No"); 
+           data.add("Si");
+       }
+       
+       for(int i = 0; i < 60; i++){
+           switch(tipoLugarEvento){
+               case "Cerrado": data.add("Si"); break;
+               case "AireLibre": data.add("No"); break;
+               case "SemiAbierto": data.add(elegirElementoRandom(SI_O_NO)); break;
+           }
+       }
+       for(int i = 0; i < 40; i++){
+           data.add(sanaDistancia == "No"? "Si" : "No");
+       }
+       for(int i = 0; i < 80; i++){
+           data.add(usoCubrebocas == "No"? "Si" : "No");
+       }
+       for(int i = 0; i < 10; i++){
+           data.add(dia == "SabadoDomingo"? "Si" : "No");
+       }
+       
+       return elegirElementoRandom( data.toArray() );
+    }
+    
+    private String elegirElementoRandom(Object [] data){
+        int index = r.nextInt(data.length);
+        return (String)data[index];
+    }
 
     public void defineEncabezado() throws IOException {
         escribeLinea("@relation covid");
         bw.newLine();
-        escribeLinea("@attribute Edad   numeric");
-        escribeLinea("@attribute Nivel  {Niño, Joven, Adulto, Anciano}");
+        escribeLinea("@attribute Edad numeric");
+        escribeLinea("@attribute Nivel {Niño, Joven, Adulto, Anciano}");
         escribeLinea("@attribute Dia {LunesViernes, SabadoDomingo}");
         escribeLinea("@attribute Turno {Matutino, Vespertino, Nocturno}");
         escribeLinea("@attribute Hora numeric");
@@ -140,17 +151,15 @@ public class GeneraArchivoARFF {
             carpeta.mkdir();
         }
 
-        if (!file.exists()) {
-            file.createNewFile();
+        if (file.exists()) {
+            file.delete();
         }
+        file.createNewFile();
 
         fw = new FileWriter(file);
         bw = new BufferedWriter(fw);
         return false;
     }
 
-    public static void main(String[] args) {
-        new GeneraArchivoARFF();
-    }
 
 }
